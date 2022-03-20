84e72b7d9b022e73dc84ee47f3f496d1a68cadab
Derik Mitchell <59582279+dmitch-lynkd@users.noreply.github.com>
2022-03-20 02:01:54 -0400

    Update pico.ts

--- src/pico.ts
+++ src/pico.ts
@@ -1,42 +1,40 @@
 // Pico Pins
-class Pico {
-  static A0: Pin;
-  static A1: Pin;
-  static A2: Pin;
-  static A3: Pin;
-  static A4: Pin;
-  static A5: Pin;
-  static A6: Pin;
-  static A7: Pin;
-  static A8: Pin;
-  static A9: Pin;
-  static A10: Pin;
-  static A11: Pin;
-  static A12: Pin;
-  static A13: Pin;
-  static A14: Pin;
-  static A15: Pin;
+let A0: Pin;
+let A1: Pin;
+let A2: Pin;
+let A3: Pin;
+let A4: Pin;
+let A5: Pin;
+let A6: Pin;
+let A7: Pin;
+let A8: Pin;
+let A9: Pin;
+let A10: Pin;
+let A11: Pin;
+let A12: Pin;
+let A13: Pin;
+let A14: Pin;
+let A15: Pin;
 
-  static B0: Pin;
-  static B1: Pin;
-  static B2: Pin;
-  static B3: Pin;
-  static B4: Pin;
-  static B5: Pin;
-  static B6: Pin;
-  static B7: Pin;
-  static B8: Pin;
-  static B9: Pin;
-  static B10: Pin;
-  static B12: Pin;
-  static B13: Pin;
-  static B14: Pin;
-  static B15: Pin;
+let B0: Pin;
+let B1: Pin;
+let B2: Pin;
+let B3: Pin;
+let B4: Pin;
+let B5: Pin;
+let B6: Pin;
+let B7: Pin;
+let B8: Pin;
+let B9: Pin;
+let B10: Pin;
+let B12: Pin;
+let B13: Pin;
+let B14: Pin;
+let B15: Pin;
 
-  static C13: Pin;
-  static C14: Pin;
-  static C15: Pin;
+let C13: Pin;
+let C14: Pin;
+let C15: Pin;
 
-  static H0: Pin;
-  static H1: Pin;
-}
+let H0: Pin;
+let H1: Pin;
